5b87f5866e7ddf3df3c4538d6fcbf58fdf18c267
tarakavian84@gmail.com <tarakavian84@gmail.com>
2022-05-08 19:37:46 +0430

    Add project files.

---
+++ S9/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter student's theory score: ");
+            int theo = int.Parse(Console.ReadLine());
+            Console.Write("Enter student's practical grade: ");
+            int prac = int.Parse(Console.ReadLine());
+            int sum = theo + prac;
+            Console.WriteLine("The student's final grade is: " + sum);
+            Console.ReadKey();
+        }
+    }
+}
